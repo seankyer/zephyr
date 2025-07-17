b95534d23290537fc0bc139386fe309dd6e257c4
Eric Hay <Eric.Hay@analog.com>
2025-07-17 16:24:57 -0700

    cpu_freq: cpu_load: Add CPU load measurement
    Implement the CPU load measurement function for
    use by the CPU frequency scaling on demand policy.
    The statistics used to calculate the load are taken
    from the scheduler. Specifically, the number of
    cycles used by the idle thread.
    
    The CPU frequency scaling subsystem does not currently
    support SMP. The CPU load measurement can be made to
    support SMP since the statistics are measured from
    the scheduler.
    
    Signed-off-by: Eric Hay <Eric.Hay@analog.com>

--- include/zephyr/cpu_freq/policy.h
+++ include/zephyr/cpu_freq/policy.h
@@ -22,11 +22,10 @@ extern "C" {
  * @brief Get the next P-state from CPU Frequency Policy
  *
  * @param p_state Pointer to the P-state struct where the next P-state is returned.
- * @param cpu_load Current CPU load percentage (0-100).
  *
  * @retval 0 In case of success, nonzero in case of failure.
  */
-uint32_t cpu_freq_policy_get_p_state_next(struct p_state *p_state, uint32_t cpu_load);
+int cpu_freq_policy_get_p_state_next(struct p_state *p_state);
 
 #ifdef __cplusplus
 }
--- subsys/cpu_freq/policies/on_demand/on_demand.c
+++ subsys/cpu_freq/policies/on_demand/on_demand.c
@@ -8,6 +8,7 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/cpu_freq/policy.h>
 #include <zephyr/cpu_freq/cpu_freq.h>
+#include <metrics/cpu_load.h>
 #include "cpu_freq_soc.h"
 
 LOG_MODULE_REGISTER(cpu_freq_policy_on_demand, CONFIG_CPU_FREQ_LOG_LEVEL);
@@ -20,8 +21,19 @@ const struct p_state *soc_p_states[] = {
  * first P-state where the cpu_load is greater than or equal to the trigger threshold
  * of the P-state.
  */
-uint32_t cpu_freq_policy_get_p_state_next(struct p_state *p_state_out, uint32_t cpu_load)
+int cpu_freq_policy_get_p_state_next(struct p_state *p_state_out)
 {
+	int ret;
+	uint32_t cpu_load;
+
+	ret = get_cpu_load(&cpu_load);
+	if (ret) {
+		LOG_ERR("Unable to retrieve CPU load");
+		return ret;
+	}
+
+	LOG_DBG("Current CPU Load: %d%%", cpu_load);
+
 	for (int i = 0; i < ARRAY_SIZE(soc_p_states); i++) {
 		const struct p_state *state = soc_p_states[i];
 
