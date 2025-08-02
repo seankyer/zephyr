e67fb37085f8f8ef1e34d3d2021afbac29adff63
Sean Kyer <Sean.Kyer@analog.com>
2025-08-01 19:44:50 -0700

    tests: cpu_freq: Add SoC and on_demand tests
    Add tests for CPU frequency subsystem covering
    SoC integration of pstate as well as the on_demand
    policy.
    
    Signed-off-by: Sean Kyer <Sean.Kyer@analog.com>

---
+++ tests/subsys/cpu_freq/cpu_freq_soc/src/main.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/cpu_freq/cpu_freq.h>
+
+#include "cpu_freq_soc.h"
+
+LOG_MODULE_REGISTER(cpu_freq_soc_test, LOG_LEVEL_INF);
+
+const struct pstate *soc_pstates_dt[] = {
+	DT_FOREACH_CHILD(DT_PATH(performance_states), PSTATE_DT_GET)};
+
+/**
+ * Test SoC integration of CPU Freq
+ */
+ZTEST(cpu_freq_soc, test_soc_pstates)
+{
+	int ret;
+
+	zassert_true(ARRAY_SIZE(soc_pstates_dt) > 0, "No P-states defined in devicetree");
+
+	LOG_INF("%d P-states defined for %s", ARRAY_SIZE(soc_pstates_dt), CONFIG_BOARD_TARGET);
+
+	for (int i = 0; i < ARRAY_SIZE(soc_pstates_dt); i++) {
+		const struct pstate *state = soc_pstates_dt[i];
+
+		/* Set performance state using pstate driver */
+		ret = cpu_freq_pstate_set(*state);
+		zassert_equal(ret, 0, "Failed to set P-state %d", i);
+	}
+}
+
+ZTEST_SUITE(cpu_freq_soc, NULL, NULL, NULL, NULL, NULL);
